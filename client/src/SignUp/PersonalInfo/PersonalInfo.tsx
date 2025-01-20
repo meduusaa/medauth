3d371d974985c94fdc33528d9cbdcf810a35b0f4
jennyyoung9 <chikamsojennifer@gmail.com>
2025-01-20 13:07:40 +0100

    implementation of passwordless flow

--- client/src/SignUp/PersonalInfo/PersonalInfo.tsx
+++ client/src/SignUp/PersonalInfo/PersonalInfo.tsx
@@ -1,15 +1,25 @@
+import { useState } from "react"
+
 const PersonalInfo = () => {
+
+    const [isValid, setIsValid] = useState(false)
+    const [isDirty, setIsDirty] = useState(false)
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setIsDirty(true)
+        setIsValid(e.target.value.length>3)
+    }
     return(
         <div>
             <div><img src="" alt="" /></div>
             <form action="">
-                <input type="text" name="" id="" placeholder="First Name"/>
-                <input type="text" name="" id="" placeholder="Last Name"/>
-                <input type="text" name="" id="" placeholder="Gender"/>
-                <input type="text" name="" id="" placeholder="Nationalityn"/>
+                <input type="text" name="" id="" placeholder="First Name" onChange={handleInputChange}/>
+                <input type="text" name="" id="" placeholder="Last Name" onChange={handleInputChange}/>
+                <input type="text" name="" id="" placeholder="Gender" onChange={handleInputChange}/>
+                <input type="text" name="" id="" placeholder="Nationality" onChange={handleInputChange}/>
                 <div>
                     <button>Previous</button>
-                    <button>Next</button>
+                    <button disabled={!isValid || !isDirty}>Next</button>
                 </div>
             </form>
         </div>
--- client/src/SignUp/ProfessionalInfo/ProfessionInfo.tsx
+++ client/src/SignUp/ProfessionalInfo/ProfessionInfo.tsx
@@ -1,16 +1,26 @@
+import { useState } from "react"
+
+
 const ProfessionInfo = () => {
-    return(
+    const [isValid, setIsValid] = useState(false)
+    const [isDirty, setIsDirty] = useState(false)
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setIsDirty(true)
+        setIsValid(e.target.value.length > 3)
+    }
+    return (
         <div>
             <div><img src="" alt="" /></div>
             <i className="fa-solid fa-arrow-left"></i>
             <form action="">
-                <input type="text" name="Profession" id="" placeholder="Profession"/>
-                <input type="text" name="Years-Experience" id="" placeholder="Years of experience"/>
-                <input type="url" name="Patron-link" id="" placeholder="Patron-link"/>
-                <input type="url" name="Linkedln-link" id="" placeholder="Linkedln-link"/>
+                <input type="text" name="Profession" id="" placeholder="Profession" onChange={handleInputChange}/>
+                <input type="text" name="Years-Experience" id="" placeholder="Years of experience" onChange={handleInputChange}/>
+                <input type="url" name="Patron-link" id="" placeholder="Patron-link" onChange={handleInputChange}/>
+                <input type="url" name="Linkedln-link" id="" placeholder="Linkedln-link" onChange={handleInputChange}/>
                 <div>
                     <button>Previous</button>
-                    <button>Next</button>
+                    <button disabled={!isValid || !isDirty}>Next</button>
                 </div>
             </form>
         </div>
